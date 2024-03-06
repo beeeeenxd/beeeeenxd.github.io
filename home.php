d0395b57ee3ef49ad73e6e6bbd780242ddba8ef0
beeeeenxd <157484662+beeeeenxd@users.noreply.github.com>
2024-03-07 00:25:50 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Benjamin Maunda </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scaled=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="boxicons.min.css">
+    <script src="script.js"></script> 
+    <link rel="shortcut icon" type="x-icon" href="img/ben.jpg">
+  
+      <!--box link --->
+      <link rel="stylesheet"href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+      <!--google link --->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200&display=swap" rel="stylesheet">
+</head>
+<body>
+  <!--header section --->
+  <header>
+      <a href="#" class="logo"><span>B</span>enjamin</a>
+
+      <ul class="navbar">
+          <li><a href="#" class="active">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Contact</a></li>
+</ul>
+
+</header>
+    <!-- home --->
+    <section class="home">
+      <div class="home-text">
+      <span> Hello I'm</span>
+      <h1>Benjamin Maunda</h1>
+        <p> Web Developer | Programmer</p>
+        <div class="main-btn">
+          <a href="ben.pdf" class="btn">Download My CV</a>
+          <a href="https://www.facebook.com/Bandrang213" class="btn">Facebook </a>
+          <a href="https://www.facebook.com/Bandrang213" class="btn">Instagram </a>
+          <a href="https://www.facebook.com/Bandrang213" class="btn">Messenger </a>
+          <a href="https://www.linkedin.com/in/benjamin-maunda-31a0972a7/" class="btn">LinkedIn </a>
+      </div>
+    </div>
+    <div class="home-img">
+      <img src="img/ben.png">
+</section>
+</body>
---
+++ script.js
@@ -0,0 +1,11 @@
+// Assuming your download button has an ID attribute, like "downloadButton"
+var downloadButton = document.getElementById('downloadButton');
+
+// Check if the button element is found
+if (downloadButton) {
+    // Simulate a click on the button
+    downloadButton.click();
+} else {
+    console.error('Download button not found');
+}
+// script.js
